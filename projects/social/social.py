d766695991a72feffeb9c77f1b8dd0df5d6fbf6d
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-04-16 10:46:19 -0700

    added social optimization

--- projects/social/social.py
+++ projects/social/social.py
@@ -35,11 +35,14 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -69,21 +72,37 @@ class SocialGraph:
         for i in range(0, num_users):
             self.add_user(f"User {i+1}")
 
-        # Generate all friendship combinations
-        possible_friendships = []
+        # New Optimized Populate Graph Method
+        target_friendships = num_users * avg_friendships // 2
+        total_friendships = 0
+        collisions = 0
 
-        # Avoid dupes by making sure first number is smaller than second
-        for user_id in self.users:
-            for friend_id in range(user_id+1, self.last_id+1):
-                possible_friendships.append((user_id, friend_id))
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
 
-        # Shuffle all possible friendships
-        random.shuffle(possible_friendships)
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
 
-        # Create for first X pairs x is total //2
-        for i in range(num_users * avg_friendships // 2):
-            friendship = possible_friendships[i]
-            self.add_friendship(friendship[0], friendship[1])
+        print('Total Collisions: ', collisions)
+        # Original Populate Graph Method (naive)
+        # # Generate all friendship combinations
+        # possible_friendships = []
+
+        # # Avoid dupes by making sure first number is smaller than second
+        # for user_id in self.users:
+        #     for friend_id in range(user_id+1, self.last_id+1):
+        #         possible_friendships.append((user_id, friend_id))
+
+        # # Shuffle all possible friendships
+        # random.shuffle(possible_friendships)
+
+        # # Create for first X pairs x is total //2
+        # for i in range(num_users * avg_friendships // 2):
+        #     friendship = possible_friendships[i]
+        #     self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
